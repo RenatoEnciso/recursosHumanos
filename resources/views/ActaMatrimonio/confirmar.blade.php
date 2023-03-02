83645066faabcea3e626e990b3ad70f06469a044
alfredo7792 <alfredo7791@gmail.com>
2023-03-01 23:57:33 -0500

    Otro Mejora :v

--- resources/views/ActaMatrimonio/confirmar.blade.php
+++ resources/views/ActaMatrimonio/confirmar.blade.php
@@ -7,12 +7,12 @@
     <h1 id="titulo" class="acta_title">Desea eliminar esta Acta de MATRIMONIO?</h1>
     <br>
     <div class="info_confirmacion">
-        <h2><b>Codigo</b> : {{$ActaMatrimonio->idActa}} <br> <b>Esposa:</b> {{$ActaMatrimonio->Persona->Nombres}} <br> <b>Esposo: </b>
+        <h2><b>Codigo</b> : {{$ActaMatrimonio->idActa}} <br> <b>Esposa:</b> {{$ActaMatrimonio->Persona->Nombres." ".$ActaMatrimonio->Persona->Apellido_Paterno." ".$ActaMatrimonio->Persona->Apellido_Materno}} <br> <b>Esposo: </b>
             @foreach ($ActasMatrimonio as $item1)
                                 @if ($item1->Persona->sexo=='F')
                                         @foreach ($ActasMatrimonio as $item2)
                                             @if (($item1->Acta->idActa)==($item2->Acta->idActa) && ($item1->Persona->DNI)!=($item2->Persona->DNI))
-                                                <td>{{$item2->Persona->Nombres." ".$item2->Persona->Apellido_Paterno }}</td>
+                                                {{$item2->Persona->Nombres." ".$item2->Persona->Apellido_Paterno." ".$item2->Persona->Apellido_Materno }}
                                             @endif
                                         @endforeach
                                 @endif

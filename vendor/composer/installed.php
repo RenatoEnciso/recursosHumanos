80b239930b7dda59025ad1c4e421e4052e5a905e
Stephano0207 <castanedastephano16@gmail.com>
2023-10-06 12:47:35 -0500

    Dependecias

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'e75b412456ab84344b2c56985283515ce9712527',
+        'reference' => 'b5a4750c523af771b561843658f64680c4e6e5bd',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -385,7 +385,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'e75b412456ab84344b2c56985283515ce9712527',
+            'reference' => 'b5a4750c523af771b561843658f64680c4e6e5bd',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),

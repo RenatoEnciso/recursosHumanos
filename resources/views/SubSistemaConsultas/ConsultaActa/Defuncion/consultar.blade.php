764872283a9d8df741bb91aa993e3f1d3d0bfe11
Stephano0207 <castanedastephano16@gmail.com>
2023-10-09 20:15:13 -0500

    Correccion de validaciones

--- resources/views/SubSistemaConsultas/ConsultaActa/Defuncion/consultar.blade.php
+++ resources/views/SubSistemaConsultas/ConsultaActa/Defuncion/consultar.blade.php
@@ -1,62 +1,62 @@
 @extends('plantillaConsultas.Acta')
-@section('titulo','Acta Defuncion')
-@section('subtitulo','Consulta Acta Defuncion')
+@section('titulo', 'Acta Defuncion')
+@section('subtitulo', 'Consulta Acta Defuncion')
 @section('cuerpo')
-    <form action="{{route('SearchDefuncion')}}" method="POST" class="px-5 py-2">
-      @csrf
+    <form action="{{ route('SearchDefuncion') }}" method="POST" class="px-5 py-2">
+        @csrf
         <div>
-            <img src="{{asset('images/defuncion.png')}}" alt="defuncion" width="100px" class="img-fluid d-block mx-auto">
+            <img src="{{ asset('images/defuncion.png') }}" alt="defuncion" width="100px" class="img-fluid d-block mx-auto">
         </div>
-       
-        
-        
-          <label class="form-label" for="">Año Defuncion</label> 
-           <input type="number" class="form-control  @error('ano') is-invalid @enderror" name="ano" placeholder="Año(4 digitos)">
-          @error('ano')
+
+
+
+        <label class="form-label" for="">Año Defuncion</label>
+        <input type="number" class="form-control  @error('ano') is-invalid @enderror" name="ano"
+            placeholder="Año(4 digitos)" value="{{ old('ano') }}">
+        @error('ano')
             <div class="invalid-feedback">
-              Ingresar el año de defuncion
+                Ingresar el año de defuncion
             </div>
-        
-          @enderror
-         <div class="row"> 
-        <div class="col-sm-12 col-md-6 ">
-          <label class="form-label" for="">Primer Apellido</label> 
-          <input type="text" class="form-control @error('primer_apellido') is-invalid @enderror" name="primer_apellido" placeholder="Primer Apellido">
-          @error('primer_apellido')
-            <div class="invalid-feedback">
-            Ingresar el primer apellido 
+        @enderror
+        <div class="row">
+            <div class="col-sm-12 col-md-6 ">
+                <label class="form-label" for="">Primer Apellido</label>
+                <input type="text" class="form-control @error('primer_apellido') is-invalid @enderror"
+                    name="primer_apellido" placeholder="Primer Apellido" value ='{{ old('primer_apellido') }}'>
+                @error('primer_apellido')
+                    <div class="invalid-feedback">
+                        Ingresar el primer apellido
+                    </div>
+                @enderror
             </div>
-        
-          @enderror
-        </div>
-        <div class="col-sm-12 col-md-6">
-          <label  class="form-label" for="">Segundo Apellido</label>
-           <input type="text" class="form-control @error('segundo_apellido') is-invalid @enderror" name="segundo_apellido" placeholder="Segundo Apellido">
-           @error('segundo_apellido')
-           <div class="invalid-feedback">
-            Ingresar el segundo apellido
-           </div>
-       
-         @enderror
-          </div>
+            <div class="col-sm-12 col-md-6">
+                <label class="form-label" for="">Segundo Apellido</label>
+                <input type="text" class="form-control @error('segundo_apellido') is-invalid @enderror"
+                    name="segundo_apellido" placeholder="Segundo Apellido" value ='{{ old('segundo_apellido') }}'>
+                @error('segundo_apellido')
+                    <div class="invalid-feedback">
+                        Ingresar el segundo apellido
+                    </div>
+                @enderror
+            </div>
         </div>
         <div class="mb-3">
-           <label class="form-label" for="" class="form-label">Prenombres</label> 
-           <input type="text" class="form-control @error('prenombres') is-invalid @enderror" name="prenombres" placeholder="Prenombres">
-           @error('prenombres')
-           <div class="invalid-feedback">
-            Ingresar los nombres del difunto
-           </div>
-       
-         @enderror
-          </div>
+            <label class="form-label" for="" class="form-label">Prenombres</label>
+            <input type="text" class="form-control @error('prenombres') is-invalid @enderror" name="prenombres"
+                placeholder="Prenombres" value ='{{ old('prenombres') }}'>
+            @error('prenombres')
+                <div class="invalid-feedback">
+                    Ingresar los nombres del difunto
+                </div>
+            @enderror
+        </div>
         <div class="row mx-5">
             <div class="col-6">
-           <button type="button" class="btn btn-danger px-5" >Salir</button> 
-        </div>
-        <div class="col-6">
-           <button type="submit"   class="btn btn-success px-3">Consultar</button>
-        </div>
+                <button type="button" class="btn btn-danger px-5">Salir</button>
+            </div>
+            <div class="col-6">
+                <button type="submit" class="btn btn-success px-3">Consultar</button>
+            </div>
         </div>
 
 
--- resources/views/SubSistemaConsultas/ConsultaActa/Nacimiento/consultar.blade.php
+++ resources/views/SubSistemaConsultas/ConsultaActa/Nacimiento/consultar.blade.php
@@ -1,59 +1,63 @@
 @extends('plantillaConsultas.Acta')
-@section('titulo','Acta Nacimiento')
-@section('subtitulo','Consulta Acta Nacimiento')
+@section('titulo', 'Acta Nacimiento')
+@section('subtitulo', 'Consulta Acta Nacimiento')
 @section('cuerpo')
-<form action="{{route('SearchNacimiento')}}" method="post" class="px-5 py-2">
-   @csrf
-   
-    <div>
-        <img src="{{asset('images/nacimiento.png')}}" alt="defuncion" width="100px" class="img-fluid d-block mx-auto">
-    </div>
-    
-      <label class="form-label" for="">Fecha de Nacimiento</label> 
-       <input type="date" class="form-control @error('fecha') is-invalid @enderror" name="fecha" placeholder="Fecha(dd/mm/yyy)">
-       @error('fecha')
-       <div class="invalid-feedback">
-        Ingresar la fecha de nacimiento
-       </div>
-       @enderror
-     <div class="row"> 
-    <div class="col-sm-12 col-md-6">
-      <label class="form-label" for="">Primer Apellido</label> 
-      <input type="text" class="form-control @error('primer_apellido') is-invalid @enderror" name="primer_apellido" placeholder="Primer Apellido">
-      @error('primer_apellido')
-      <div class="invalid-feedback">
-       Ingresar el primer apellido
-      </div>
-      @enderror
-    </div>
-    <div class="col-sm-12 col-md-6">
-      <label  class="form-label" for="">Segundo Apellido</label>
-       <input type="text" class="form-control @error('segundo_apellido') is-invalid @enderror" name="segundo_apellido" placeholder="Segundo Apellido">
-       @error('segundo_apellido')
-       <div class="invalid-feedback">
-        Ingresar el segundo apellido
-       </div>
-       @enderror
-    </div>
-    </div>
-    <div class="mb-3">
-       <label class="form-label" for="" class="form-label">Prenombres</label> 
-       <input type="text" class="form-control @error('prenombres') is-invalid @enderror" name="prenombres" placeholder="Prenombres">
-       @error('prenombres')
-       <div class="invalid-feedback">
-        Ingresar el nombres del nacido
-       </div>
-       @enderror
-    </div>
-    <div class="row mx-5">
-        <div class="col-6">
-       <button type="" class="btn btn-danger px-5" >Salir</button> 
-    </div>
-    <div class="col-6">
-       <button type="submit" class="btn btn-success px-3">Consultar</button>
-    </div>
-    </div>
+    <form action="{{ route('SearchNacimiento') }}" method="post" class="px-5 py-2">
+        @csrf
+
+        <div>
+            <img src="{{ asset('images/nacimiento.png') }}" alt="defuncion" width="100px" class="img-fluid d-block mx-auto">
+        </div>
+
+        <label class="form-label" for="">Fecha de Nacimiento</label>
+        <input type="date" class="form-control @error('fecha') is-invalid @enderror" name="fecha"
+            placeholder="Fecha(dd/mm/yyy)" value="{{old('fecha')}}">
+        @error('fecha')
+            <div class="invalid-feedback">
+                Ingresar la fecha de nacimiento
+            </div>
+        @enderror
+        <div class="row">
+            <div class="col-sm-12 col-md-6">
+                <label class="form-label" for="">Primer Apellido</label>
+                <input type="text" class="form-control @error('primer_apellido') is-invalid @enderror"
+                    name="primer_apellido" placeholder="Primer Apellido" value="{{old('primer_apellido')}}">
+                @error('primer_apellido')
+                    <div class="invalid-feedback">
+                        Ingresar el primer apellido
+                    </div>
+                @enderror
+            </div>
+            <div class="col-sm-12 col-md-6">
+                <label class="form-label" for="">Segundo Apellido</label>
+                <input type="text" class="form-control @error('segundo_apellido') is-invalid @enderror"
+                    name="segundo_apellido" placeholder="Segundo Apellido" value="{{old('segundo_apellido')}}">
+                @error('segundo_apellido')
+                    <div class="invalid-feedback">
+                        Ingresar el segundo apellido
+                    </div>
+                @enderror
+            </div>
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="" class="form-label">Prenombres</label>
+            <input type="text" class="form-control @error('prenombres') is-invalid @enderror" name="prenombres"
+                placeholder="Prenombres" value="{{old('prenombres')}}" >
+            @error('prenombres')
+                <div class="invalid-feedback">
+                    Ingresar el nombres del nacido
+                </div>
+            @enderror
+        </div>
+        <div class="row mx-5">
+            <div class="col-6">
+                <button type="" class="btn btn-danger px-5">Salir</button>
+            </div>
+            <div class="col-6">
+                <button type="submit" class="btn btn-success px-3">Consultar</button>
+            </div>
+        </div>
 
 
-</form>
+    </form>
 @endsection
